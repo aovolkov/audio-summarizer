f1c67e4fd5338b52f622359d6a61c553b9524428
Sergei <sergey.vakunov@glowbyteconsulting.com>
2023-04-17 14:06:40 +0300

    'manual download of pretrained models for whisper'

--- models/model_downloader.py
+++ models/model_downloader.py
@@ -1,15 +1,30 @@
 """
 При вызове функции в консоли при незагруженных моделях
 падает с ошибкой из-за некрректного ключа шифрования SHA256.
-Протестировано на модели large-v2  
+Протестировано на модели large-v2. Корректно загрузились tiny и base.
 """
 
 
 import sys
+from pathlib import Path
+import os
 from whisper import _download, _MODELS
 
-models = ["tiny", "base", "small", "medium", "large"]
+
+_PATH = os.path.join(Path.home(), '.cache', 'whisper')
+
+models = [#"tiny.en",
+          "tiny",
+          #"base.en",
+          "base",
+          #"small.en",
+          "small",
+          #"medium.en",
+          "medium",
+          "large"]
 
 for model in models:
     print(f'Downloading model {model}....')
-    _download(_MODELS[model], "~/.cache/whisper", False)
+    _download(_MODELS[model],
+              _PATH,
+              False)
